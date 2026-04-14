39f731ad2d462db38bab789d6ea07c9faca51978
GeolearnIndia <lakshaydhankhar8@gmail.com>
2026-04-14 22:29:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function openGIS(){
+document.querySelector('.popup').style.display='none';
+document.getElementById('gis').style.display='block';
+}
+
+function openRS(){
+document.querySelector('.popup').style.display='none';
+document.getElementById('rs').style.display='block';
+}
