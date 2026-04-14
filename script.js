fd0171d0084dda3d1ec9a9c5bd7f3ff5c68f38d6
GeolearnIndia <lakshaydhankhar8@gmail.com>
2026-04-14 23:02:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function openGIS(){
 document.querySelector('.popup').style.display='none';
 document.getElementById('gis').style.display='block';
 }
-
 function openRS(){
 document.querySelector('.popup').style.display='none';
 document.getElementById('rs').style.display='block';
